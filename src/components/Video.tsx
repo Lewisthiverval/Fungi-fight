a5660ff28569a01c09a0386af8335cd84cf96e3b
Lewisthiverval <54368129+Lewisthiverval@users.noreply.github.com>
2023-01-08 20:55:36 +0000

    video add

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 // import "../styles/App.css";
 
-//VIDEO STREAM
+// import video from "../assets/video.mp4";
+
 export function Video() {
-  return <div className="video"> Video Stream</div>;
+  return (
+    <div className="videoContainer">
+      <div className="video">
+        <video controls autoPlay>
+          <source src="src/assets/video.mp4" type="video/mp4" />
+          this browser does not display the video tag
+        </video>
+      </div>
+    </div>
+  );
 }
